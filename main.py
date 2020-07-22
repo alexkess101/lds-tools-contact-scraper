c9083ea901ea44f22ad17820e004c6733c0ccef1
Alex Kessinger <alex@smartystreets.com>
2020-07-21 21:53:47 -0600

    get info doesn't loop but grabs the right links

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Activate venv
-# source *dirName*/bin/activate
+# source bin/activate
 #
 # Deactivate venv
 # deactivate
@@ -10,33 +10,39 @@ from credentials import *
 PATH = "/usr/local/bin/chromedriver.exe"
 
 class script():
-     def __init__(self, username, password):
+    def __init__(self, username, password):
         self.driver = webdriver.Chrome()
         self.username = username
         self.driver.get("https://www.churchofjesuschrist.org/?lang=eng")
-        sleep(1)
+        sleep(2)
         self.driver.find_element_by_xpath("/html/body/div[2]/header/div[2]/div/ul/li[2]").click()
         self.driver.find_element_by_xpath("//a[contains(text(), 'Sign In')]").click()
         sleep(2)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
         self.driver.find_element_by_xpath('//input[@type="submit"]').click()
-        sleep(1)
+        sleep(2)
         self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
         self.driver.find_element_by_xpath('//input[@type="submit"]').click()
         sleep(2)
-        mobile_menu_element = self.driver.find_element_by_xpath("/html/body/div[2]/header/div[3]/span[2]/span")
-        desktop_menu_element = self.driver.find_element_by_xpath("/html/body/div[2]/header/div[2]/div/ul/li[2]")
-
-        if mobile_menu_element:
-            mobile_menu_element.click()
-            self.driver.find_element_by_xpath("/html/body/div[2]/header/div[3]/span[2]/div/div[2]/a[3]").click()
-        else:
-            desktop_menu_element.click()
-            self.driver.find_element_by_xpath("/html/body/div[2]/header/div[2]/div/ul/li[2]/div/div[2]/div[2]/div/div[2]/a[3]").click()
-            
+        # self.driver.find_element_by_xpath("/html/body/div[2]/header/div[2]/div/ul/li[2]").click()
+        # self.driver.find_element_by_xpath("//a[contains(text(), 'Ward Directory and Map')]").click()
+        self.driver.get("https://directory.churchofjesuschrist.org/")
         sleep(3)
+
+    def getInfo(self, index):
+        dataList = []
+        links = self.driver.find_elements_by_class_name("ListingEntry__HouseholdEntry-sc-1tjfxc9-0")
+        # for link in links:
+        dataItem = {}
+        links[index].click()
+        sleep(1)
+        dataItem["name"] = self.driver.find_element_by_class_name("Page__Heading-r1ca7-6").text
+        dataItem["number"] = self.driver.find_element_by_class_name("bspPTM").text #all items have this class
+        dataList.append(dataItem)
+        print(dataList)
+        self.driver.find_element_by_xpath("/html/body/div[2]/churchofjesuschrist-eden-normalize/main/div/section/div[2]/section/button").click()
+        sleep(1)
+
 
 
 bot = script(username, password)
-
-
